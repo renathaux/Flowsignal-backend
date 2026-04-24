89892f509970b0c4943c3f2d17f07af3f2055464
renathaux <charlesdieufort2@gmail.com>
2026-04-23 21:38:01 -0400

    Add files via upload

--- Backend/api.py
+++ Backend/api.py
@@ -16,17 +16,14 @@ def start_background_task():
     thread = threading.Thread(target=background_fetch)
     thread.daemon = True
     thread.start()
-app.add_middleware(
+
+    app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5501",
-        "http://localhost:5501",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 class TradeRequest(BaseModel):
     symbol: str
